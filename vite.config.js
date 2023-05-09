f596d7e158d1ee5f3cf75cc485d403fcd2e84f8c
Mike Weaver <michael@triplewhale.com>
2023-05-09 11:29:37 -0400

    Fix build!!

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
 import { VitePluginNode } from "vite-plugin-node";
+import packageJson from "./package.json";
+
+// https://stackoverflow.com/a/76176889
+const dependencies = Object.keys({
+  ...packageJson.dependencies,
+  ...packageJson.devDependencies,
+});
+
+const noExternal = process.env.NODE_ENV === "production" ? dependencies : [];
 
 export default defineConfig({
   plugins: [
@@ -14,5 +23,8 @@ export default defineConfig({
         manualChunks: {},
       },
     },
+  },
+  ssr: {
+    noExternal,
   },
 });
