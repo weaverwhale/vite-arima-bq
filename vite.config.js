8481f3fec48b720deef7e86fd6653a43f3608373
Mike Weaver <michael@triplewhale.com>
2023-05-18 09:23:06 -0400

    compile module instead of common

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        format: "commonjs",
+        format: "module",
         manualChunks: {},
       },
     },
