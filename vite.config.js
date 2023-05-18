013defc7b673261bed9dcfd48b87470b47ad7041
Mike Weaver <michael@triplewhale.com>
2023-05-18 10:15:54 -0400

    undo

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        format: "module",
+        format: "commonjs",
         manualChunks: {},
       },
     },
